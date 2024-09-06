4583186b90651d92707b3d167268a23bbb4e5d1c
Ashish Maurya <ashish.1999vns@gmail.com>
2024-09-06 13:20:42 +0530

    Fixes unecessary exports

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,11 +6,6 @@ import { Label } from "@/components/ui/label"
 
 import { login } from './action'
 
-
-
-export const description =
-  "A login page with two columns. The first column has the login form with email and password. There's a Forgot your passwork link and a link to sign up if you do not have an account. The second column has a cover image."
-
 function Page() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
