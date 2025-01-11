edf7d3e330c6b9110a3bf90c36be21082f8b2667
Ashish Maurya <ashish.1999vns@gmail.com>
2025-01-12 01:52:21 +0530

    added login and signup assets

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,11 +34,11 @@ function Page() {
       </div>
       <div className="hidden bg-muted lg:block">
         <Image
-          src="/placeholder.svg"
+          src="/assets/login.jpg"
           alt="Image"
           width="1920"
           height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="h-full w-full object-cover dark:brightness-[0.7] dark:grayscale"
         />
       </div>
     </div>
