8b2bd4fb995d85d8faf3116f9155be56528c1e14
Ashish Maurya <ashish.1999vns@gmail.com>
2025-01-12 12:22:50 +0530

    Disabled the posthog in local env

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,7 +3,11 @@
 import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
 
-if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+if (
+  typeof window !== 'undefined' &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+  process.env.NODE_ENV !== 'development'
+) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
@@ -15,8 +19,10 @@ if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
+    console.info('PostHogProvider disabled in development');
     return <>{children}</>;
   }
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
