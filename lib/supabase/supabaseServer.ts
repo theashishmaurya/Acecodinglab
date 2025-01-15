976737f45db552fef4d9327e97f3bff5e5002fd1
Ashish Maurya <ashish.1999vns@gmail.com>
2025-01-15 23:14:51 +0530

    server type

--- lib/supabase/supabaseServer.ts
+++ lib/supabase/supabaseServer.ts
@@ -2,7 +2,7 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies, type UnsafeUnwrappedCookies } from 'next/headers';
 
 export default function createClient() {
-  const cookieStore = (cookies() as unknown as UnsafeUnwrappedCookies)
+  const cookieStore = ((cookies() as unknown as UnsafeUnwrappedCookies) as unknown as UnsafeUnwrappedCookies)
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
