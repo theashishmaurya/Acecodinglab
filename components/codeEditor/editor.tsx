f7395f2493b3ed22a4a016ce88deda4ce996d164
Ashish Maurya <ashish.1999vns@gmail.com>
2025-02-13 01:52:16 +0530

    Refactored the Editor

--- components/codeEditor/editor.tsx
+++ components/codeEditor/editor.tsx
@@ -6,11 +6,10 @@ import {
   SandpackConsole,
   SandpackLayout,
   SandpackPreview,
-  SandpackStack,
   SandpackTests,
   useSandpack,
 } from '@codesandbox/sandpack-react';
-import React, { memo, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import { practiceSessionsAPI } from '@/db/practiceSession/practiceSession.client';
@@ -18,30 +17,23 @@ import { useParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { Spec } from '@codesandbox/sandpack-react/components/Tests/Specs';
 import { useSideNav } from '@/app/lab/sideNav.provider';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '../ui/resizable';
 
 interface CustomBottomPanel {
   consoleVisibility: boolean;
-  verticalSize: number;
 }
 
 const CustomBottomPanel = (props: CustomBottomPanel) => {
-  const { consoleVisibility, verticalSize } = props;
+  const { consoleVisibility } = props;
   const { toggleTestPanel, onTestComplete } = useSideNav();
 
   return (
     <>
-      <div
-        className="console-wrapper w-full overflow-hidden"
-        style={{
-          flexGrow: consoleVisibility ? 100 - verticalSize : 0,
-          flexShrink: consoleVisibility ? 100 - verticalSize : 0,
-          flexBasis: 0,
-          width: '100%',
-          maxHeight: consoleVisibility
-            ? `calc(${100 - verticalSize}% - 1px)`
-            : 0,
-        }}
-      >
+      <div className="console-wrapper w-full overflow-hidden h-full">
         {!toggleTestPanel && (
           <SandpackConsole
             className={classNames('overflow-y', [
@@ -87,11 +79,6 @@ const CustomEditor = (props: ICustomPreview) => {
 function Editor({ isSample }: { isSample: boolean }) {
   const [consoleVisibility, setConsoleVisibility] = React.useState(true);
   const [counter, setCouter] = useState(0);
-  const dragEventTargetRef = React.useRef<any>(null);
-  const [horizontalSize, setHorizontalSize] = React.useState(50); // 50% of the screen
-  const [verticalSize, setVerticalSize] = React.useState(70);
-  const [testResults, setTestResults] = useState<Record<string, Spec>>({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const { sandpack } = useSandpack();
   const params = useParams();
   const slug = params.slug as string;
@@ -116,31 +103,10 @@ function Editor({ isSample }: { isSample: boolean }) {
     }
   }, [sandpack.files, debouncedUpdateCode, isSample]);
 
-  const RightColumn = SandpackStack;
-
-  const MenuColumn = SandpackStack;
-
-  const rightColumnStyle = {
-    flexGrow: 100 - horizontalSize,
-    flexShrink: 100 - horizontalSize,
-    flexBasis: 0,
-    width: 100 + '%',
-    display: 'flex',
-    gap: consoleVisibility ? 1 : 0,
-    height: '100%',
-  };
-
   const topRowStyle = {
-    flexGrow: verticalSize,
-    flexShrink: verticalSize,
     flexBasis: 0,
     overflow: 'hidden',
     height: '100%',
-  };
-
-  const rightColumnProps = {
-    className: '.sp' + '-preset-column',
-    style: rightColumnStyle,
   };
 
   const actionsChildren = (
@@ -153,141 +119,42 @@ function Editor({ isSample }: { isSample: boolean }) {
       }}
     />
   );
-  const onDragMove = (event: MouseEvent): void => {
-    if (!dragEventTargetRef.current) return;
-
-    const container = dragEventTargetRef.current.parentElement as
-      | HTMLDivElement
-      | undefined;
-
-    if (!container) return;
-
-    const direction = dragEventTargetRef.current.dataset.direction as
-      | 'horizontal'
-      | 'vertical';
-    const isHorizontal = direction === 'horizontal';
-
-    const { left, top, height, width } = container.getBoundingClientRect();
-    const offset = isHorizontal
-      ? ((event.clientX - left) / width) * 100
-      : ((event.clientY - top) / height) * 100;
-    const boundaries = Math.min(Math.max(offset, 25), 75);
-
-    if (isHorizontal) {
-      setHorizontalSize(boundaries);
-    } else {
-      setVerticalSize(boundaries);
-    }
-
-    container.querySelectorAll(`.sp-stack`).forEach(item => {
-      (item as HTMLDivElement).style.pointerEvents = 'none';
-    });
-  };
-
-  const stopDragging = (): void => {
-    const container = dragEventTargetRef.current?.parentElement as
-      | HTMLDivElement
-      | undefined;
-
-    if (!container) return;
-
-    container.querySelectorAll(`.sp-stack`).forEach(item => {
-      (item as HTMLDivElement).style.pointerEvents = '';
-    });
-
-    dragEventTargetRef.current = null;
-  };
-
-  React.useEffect(() => {
-    document.body.addEventListener('mousemove', onDragMove);
-    document.body.addEventListener('mouseup', stopDragging);
-
-    return (): void => {
-      document.body.removeEventListener('mousemove', onDragMove);
-      document.body.removeEventListener('mouseup', stopDragging);
-    };
-  }, []);
-
-  const handleTestComplete = (specs: Record<string, Spec>) => {
-    setTestResults(specs);
-
-    if (isSubmitting) {
-      const allTestsPassed = Object.values(specs).every(file =>
-        Object.values(file.tests).every(test => test.status === 'pass'),
-      );
-
-      if (allTestsPassed) {
-        console.log(specs, 'Submitted to backend');
-      } else {
-        setIsSubmitting(false);
-        // Optionally, show a message that not all tests passed
-      }
-    }
-  };
 
   const EditorStyle = {
     height: '100%', // use the original editor height
-    flexGrow: horizontalSize,
-    flexShrink: horizontalSize,
-    flexBasis: 0,
   };
 
   return (
-    <div className="flex flex-col w-full">
+    <ResizablePanelGroup direction="horizontal" className=" rounded-lg border ">
       <SandpackLayout
         style={{
           height: '88vh',
         }}
       >
-        {/* {showFile ? <SandpackFileExplorer style={{ height: '100%' }} /> : null} */}
-
-        <CustomEditor style={EditorStyle} />
-
-        <div
-          className={classNames('resize-handler', [])}
-          style={{
-            left: `calc(${horizontalSize}% - 5px)`,
-            width: 10,
-            cursor: 'ew-resize',
-          }}
-          data-direction="horizontal"
-          onMouseDown={(event): void => {
-            dragEventTargetRef.current = event.target;
-          }}
-        />
-        <RightColumn {...rightColumnProps}>
-          <SandpackPreview
-            actionsChildren={actionsChildren}
-            style={topRowStyle}
-            showNavigator={true}
-            showOpenInCodeSandbox={false}
-            showRefreshButton={true}
-            showSandpackErrorOverlay={true}
-          />
-
-          <div
-            className={classNames('resize-handler', [
-              // dragHandler({ direction: "vertical" }),
-            ])}
-            data-direction="vertical"
-            onMouseDown={(event): void => {
-              dragEventTargetRef.current = event.target;
-            }}
-            style={{
-              top: `calc(${verticalSize}% - 5px)`,
-              cursor: 'ns-resize',
-              width: 100 + '%',
-              height: 10,
-            }}
-          />
-
-          <CustomBottomPanel
-            consoleVisibility={consoleVisibility}
-            verticalSize={verticalSize}
-          />
-        </RightColumn>
+        <ResizablePanel defaultSize={50}>
+          <CustomEditor style={EditorStyle} />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel>
+              <SandpackPreview
+                actionsChildren={actionsChildren}
+                style={topRowStyle}
+                showNavigator={true}
+                showOpenInCodeSandbox={false}
+                showRefreshButton={true}
+                showSandpackErrorOverlay={true}
+              />
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel>
+              <CustomBottomPanel consoleVisibility={consoleVisibility} />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
       </SandpackLayout>
-    </div>
+    </ResizablePanelGroup>
   );
 }
 
